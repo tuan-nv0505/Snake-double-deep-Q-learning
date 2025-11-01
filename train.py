f43e8d19a28bf41b0c5e9af5dd6d0eb50d2c7a6e
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2025-11-02 00:36:41 +0700

    fix dim

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ def select_action(state_space, state_logic, agent, epsilon):
         return action
     else:
         with torch.no_grad():
-            q_values = agent(state_space.unsqueeze(1), state_logic.unsqueeze(1))
+            q_values = agent(state_space.unsqueeze(0), state_logic.unsqueeze(0))
             action = torch.argmax(q_values).item()
             return action
 
