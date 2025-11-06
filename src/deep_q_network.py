3978f059cc60fd6b16907f8a2ee0a350e8fa00ce
tuan0505 <tuan557552@gmail.com>
2025-11-07 00:06:37 +0700

    update

--- src/deep_q_network.py
+++ src/deep_q_network.py
@@ -5,14 +5,12 @@ class DeepQNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Sequential(nn.Linear(11, 128), nn.LeakyReLU())
-        self.fc2 = nn.Sequential(nn.Linear(128, 256), nn.LeakyReLU())
-        self.fc3 = nn.Sequential(nn.Linear(256, 3))
+        self.fc2 = nn.Sequential(nn.Linear(128, 3))
 
 
     def forward(self, input):
         output = self.fc1(input)
         output = self.fc2(output)
-        output = self.fc3(output)
 
         return output
 
